0076166831a0c363bfbce5e86cc82b3583c6476f
sutriono <soetrion@gmail.com>
2026-02-01 04:11:20 +0700

    Update main.go

--- main.go
+++ main.go
@@ -56,7 +56,7 @@ func main() {
 	http.HandleFunc("/api/produk/", productHandler.HandleProductByID)
 
 	// localhost:8080/health
-	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(map[string]string{
 			"status":  "OK",
